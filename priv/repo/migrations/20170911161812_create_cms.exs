302ff5421bb13f7bbd0cf788ec33c2e707a2f6ec
Carlos Saito <c.saito.style@gmail.com>
2017-09-11 19:41:35 +0200

    Add all Ontag migrations

---
+++ priv/repo/migrations/20170911161812_create_cms.exs
@@ -0,0 +1,45 @@
+defmodule OntagCore.Repo.Migrations.CreateCms do
+  use Ecto.Migration
+
+  def change do
+    create table(:cms_authors) do
+      add :role, :string
+      add :user_id, references(:users, on_delete: :delete_all),
+        null: false
+
+      timestamps()
+    end
+
+    create table(:entries) do
+      add :title, :string
+      add :entry_type, :string
+      add :author_id, references(:cms_authors),
+        null: false
+
+      timestamps()
+    end
+
+    create table(:medium_posts) do
+      add :title, :string
+      add :uri, :string
+      add :publishing_date, :utc_datetime
+      add :license, :string
+      add :tags, {:array, :string}
+      add :entry_id, references(:entries), null: false
+
+      timestamps()
+    end
+
+    create table(:external_htmls) do
+      add :uri, :string
+      add :entry_id, references(:entries), null: false
+
+      timestamps()
+    end
+
+    create unique_index(:cms_authors, [:user_id])
+    create index(:entries, [:author_id])
+    create index(:medium_posts, [:entry_id])
+    create index(:external_htmls, [:entry_id])
+  end
+end
---
+++ priv/repo/migrations/20170911164127_create_qams.exs
@@ -0,0 +1,58 @@
+defmodule OntagCore.Repo.Migrations.CreateQams do
+  use Ecto.Migration
+
+  def change do
+    create table(:qams_authors) do
+      add :role, :string
+      add :user_id, references(:users, on_delete: :delete_all),
+        null: false
+
+      timestamps()
+    end
+
+    create table(:tags) do
+      add :title, :string
+
+      timestamps()
+    end
+
+    create table(:questions) do
+      add :title, :string
+      add :author_id, references(:qams_authors),
+        null: false
+
+      timestamps()
+    end
+
+    create table(:questions_tags) do
+      add :question_id, references(:questions),
+        null: false
+      add :tag_id, references(:tags),
+        null: false
+      add :required, :boolean
+      add :author_id, references(:qams_authors),
+        null: false
+
+      timestamps()
+    end
+
+    create table(:annotations) do
+      add :entry_id, references(:entries),
+        null: false
+      add :type, :string
+      add :target, :map
+      add :tag_id, references(:tags),
+        null: false
+      add :author_id, references(:qams_authors),
+        null: false
+
+      timestamps()
+    end
+
+    create unique_index(:qams_authors, [:user_id])
+    create index(:questions, [:author_id])
+    create index(:questions_tags, [:author_id])
+    create index(:annotations, [:entry_id, :tag_id, :author_id])
+
+  end
+end
