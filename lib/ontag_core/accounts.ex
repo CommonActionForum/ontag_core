9bd56ddbdd3546fb5d3ae1fc88d3927a783fee24
Carlos Saito <c.saito.style@gmail.com>
2017-09-14 11:52:47 +0200

    Allow User authentication via PasswordCredential.
    Implement functions to authenticate users using their PasswordCredential data,
    i.e., e-mail and password.

--- lib/ontag_core/accounts.ex
+++ lib/ontag_core/accounts.ex
@@ -29,4 +29,50 @@ defmodule OntagCore.Accounts do
       :password_credential, with: &PasswordCredential.changeset/2)
     |> Repo.insert()
   end
+
+  @doc """
+  Get a `User` given its PasswordCredential data
+  """
+  def authenticate_with_password_credential(email, password) do
+    email
+    |> get_password_credential_from_email()
+    |> check_password(password)
+    |> get_user_from_password_credential()
+  end
+
+  @doc """
+  Get a `PasswordCredential` given its email
+  """
+  def get_password_credential_from_email(email) do
+    case Repo.get_by(PasswordCredential, email: email) do
+      %PasswordCredential{} = pc ->
+        {:ok, pc}
+      nil ->
+        {:error, :unauthorized}
+    end
+  end
+
+  @doc """
+  Check if, a given `PasswordCredential` has a certain password.
+
+  Returns `{:ok, pc}` on success, and `{:error, :unauthorized}` on failure.
+  """
+  def check_password({:ok, %PasswordCredential{} = pc}, password) do
+    case Comeonin.Bcrypt.check_pass(pc, password) do
+      {:error, _} ->
+        {:error, :unauthorized}
+      {:ok, pc} ->
+        {:ok, pc}
+    end
+  end
+  def check_password(any, _), do: any
+
+  @doc """
+  Get the user associated with a password credential.
+  """
+  def get_user_from_password_credential({:ok, %PasswordCredential{} = pc}) do
+    pc = Repo.preload(pc, :user)
+    {:ok, pc.user}
+  end
+  def get_user_from_password_credential(any), do: any
 end
--- test/ontag_core/accounts_test.exs
+++ test/ontag_core/accounts_test.exs
@@ -1,7 +1,8 @@
 defmodule OntagCore.AccountsTest do
   use OntagCore.DataCase
   alias OntagCore.Accounts
-  alias OntagCore.Accounts.User
+  alias OntagCore.Accounts.{User,
+                            PasswordCredential}
   @moduledoc """
   Test for `OntagCore.Accounts`
   """
@@ -28,6 +29,74 @@ defmodule OntagCore.AccountsTest do
 
     assert {:ok, %User{} = user} = Accounts.create_user(params)
     assert user.username == "john_example"
+  end
+
+  test "Get password credential given its e-mail" do
+    params = %{
+      username: "john_example",
+      name: "John example",
+      password_credential: %{
+        email: "john@example.com",
+        password: "1234"
+      }
+    }
+
+    assert {:ok, _} = Accounts.create_user(params)
+    assert {:ok, %PasswordCredential{}} =
+      Accounts.get_password_credential_from_email("john@example.com")
+  end
+
+  test "Check password validity" do
+    params = %{
+      username: "john_example",
+      name: "John example",
+      password_credential: %{
+        email: "john@example.com",
+        password: "1234"
+      }
+    }
+
+    assert {:ok, _} = Accounts.create_user(params)
+    assert {:ok, %PasswordCredential{} = pc} =
+      Accounts.get_password_credential_from_email("john@example.com")
+    assert {:ok, pc} == Accounts.check_password({:ok, pc}, "1234")
+  end
+
+  test "Get user from PasswordCredential" do
+    params = %{
+      username: "john_example",
+      name: "John example",
+      password_credential: %{
+        email: "john@example.com",
+        password: "1234"
+      }
+    }
+
+    assert {:ok, user} = Accounts.create_user(params)
+    assert {:ok, %PasswordCredential{} = pc} =
+      Accounts.get_password_credential_from_email("john@example.com")
+    assert {:ok, user2} = Accounts.get_user_from_password_credential({:ok, pc})
+
+    assert Map.drop(user, [:password_credential]) ==
+      Map.drop(user2, [:password_credential])
+  end
+
+  test "Authenticate a user" do
+    params = %{
+      username: "john_example",
+      name: "John example",
+      password_credential: %{
+        email: "john@example.com",
+        password: "1234"
+      }
+    }
+
+    assert {:ok, user} = Accounts.create_user(params)
+    assert {:ok, user2} =
+      Accounts.authenticate_with_password_credential("john@example.com", "1234")
+
+    assert Map.drop(user, [:password_credential]) ==
+      Map.drop(user2, [:password_credential])
   end
 
   test "Fail to create a duplicated user (same username)" do
@@ -66,5 +135,35 @@ defmodule OntagCore.AccountsTest do
         email: ["has already been taken"]
       }
     } = errors_on(changeset)
+  end
+
+  test "Fail to authenticate a user (e-mail not found)" do
+    params = %{
+      username: "john_example",
+      name: "John example",
+      password_credential: %{
+        email: "john@example.com",
+        password: "1234"
+      }
+    }
+
+    assert {:ok, _} = Accounts.create_user(params)
+    assert {:error, :unauthorized} ==
+      Accounts.authenticate_with_password_credential("maria@example.com", "1234")
+  end
+
+  test "Fail to authenticate a user (password do not match)" do
+    params = %{
+      username: "john_example",
+      name: "John example",
+      password_credential: %{
+        email: "john@example.com",
+        password: "1234"
+      }
+    }
+
+    assert {:ok, _} = Accounts.create_user(params)
+    assert {:error, :unauthorized} ==
+      Accounts.authenticate_with_password_credential("john@example.com", "12345")
   end
 end
