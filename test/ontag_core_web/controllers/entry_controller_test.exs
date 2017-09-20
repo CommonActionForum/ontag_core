f5b56a1c4d6ead8d5f74bf1a8add2de5521e49bd
Carlos Saito <c.saito.style@gmail.com>
2017-09-21 00:13:31 +0200

    Refactor AnnotationController

--- test/ontag_core_web/controllers/entry_controller_test.exs
+++ test/ontag_core_web/controllers/entry_controller_test.exs
@@ -1,42 +1,38 @@
 defmodule OntagCoreWeb.EntryControllerTest do
   use OntagCoreWeb.ConnCase
-  alias OntagCore.{Accounts,
-                   Guardian}
+  alias OntagCore.Guardian
 
-  test "POST /entries without a valid authorization header" do
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "none")
-      |> post(entry_path(build_conn(), :create, %{}))
-
-    assert json_response(conn, :unauthorized)
-  end
-
-  test "POST /entries with a non-valid token" do
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "Bearer none")
-      |> post(entry_path(build_conn(), :create, %{}))
-
-    assert json_response(conn, :unauthorized)
-  end
-
-  test "POST /entries with wrong parameters" do
-    {:ok, user} = Accounts.create_user(%{username: "john"})
+  setup do
+    user = create_test_user()
     {:ok, token, _} = Guardian.encode_and_sign(user)
 
-    conn =
+    wrong_header =
+      build_conn()
+      |> put_req_header("authorization", "none")
+
+    wrong_token =
+      build_conn()
+      |> put_req_header("authorization", "Bearer none")
+
+    valid_token =
       build_conn()
       |> put_req_header("authorization", "Bearer #{token}")
-      |> post(entry_path(build_conn(), :create, %{}))
 
+    world = %{
+      wrong_header: wrong_header,
+      wrong_token: wrong_token,
+      valid_token: valid_token
+    }
+
+    {:ok, world}
+  end
+
+  test "POST /entries with wrong parameters", %{valid_token: conn} do
+    conn = post(conn, entry_path(build_conn(), :create, %{}))
     assert json_response(conn, :unprocessable_entity)
   end
 
-  test "POST /entries with right parameters" do
-    {:ok, user} = Accounts.create_user(%{username: "john"})
-    {:ok, token, _} = Guardian.encode_and_sign(user)
-
+  test "POST /entries with right parameters", %{valid_token: conn} do
     params = %{
       title: "example",
       entry_type: "medium_post",
@@ -49,11 +45,7 @@ defmodule OntagCoreWeb.EntryControllerTest do
       }
     }
 
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "Bearer #{token}")
-      |> post(entry_path(build_conn(), :create, params))
-
+    conn = post(conn, entry_path(build_conn(), :create, params))
     assert json_response(conn, :created)
   end
 end
