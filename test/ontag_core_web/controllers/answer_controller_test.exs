a99af288dbc4b24bd2953a86010c5993b416b7bb
Carlos Saito <c.saito.style@gmail.com>
2017-09-21 00:25:09 +0200

    Refactor AnswerController test

--- test/ontag_core_web/controllers/answer_controller_test.exs
+++ test/ontag_core_web/controllers/answer_controller_test.exs
@@ -1,66 +1,44 @@
 defmodule OntagCoreWeb.AnswerControllerTest do
   use OntagCoreWeb.ConnCase
-  alias OntagCore.{QAMS,
-                   Accounts,
-                   Guardian}
+  alias OntagCore.Guardian
 
-  test "POST /answers without a valid authorization header" do
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "none")
-      |> post(answer_path(build_conn(), :create, %{}))
-
-    assert json_response(conn, :unauthorized)
-  end
-
-  test "POST /answers with a non-valid token" do
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "Bearer none")
-      |> post(answer_path(build_conn(), :create, %{}))
-
-    assert json_response(conn, :unauthorized)
-  end
-
-  test "POST /answers with wrong parameters" do
-    {:ok, user} = Accounts.create_user(%{username: "john"})
+  setup do
+    user = create_test_user()
     {:ok, token, _} = Guardian.encode_and_sign(user)
 
-    conn =
+    valid_token =
       build_conn()
       |> put_req_header("authorization", "Bearer #{token}")
-      |> post(answer_path(build_conn(), :create, %{}))
 
+    author = create_test_qams_author(user)
+    entry = create_test_entry()
+    tag = create_test_tag(author)
+    question = create_test_question(author)
+    annotation = create_test_annotation(author, entry, tag)
+
+    world = %{
+      valid_token: valid_token,
+      question: question,
+      annotation: annotation
+    }
+
+    {:ok, world}
+  end
+
+  test "POST /answers with wrong parameters", %{valid_token: conn} do
+    conn = post(conn, answer_path(build_conn(), :create, %{}))
     assert json_response(conn, :bad_request)
   end
 
-  test "POST /answers with right parameters" do
-    {:ok, user} = Accounts.create_user(%{username: "john"})
-    {:ok, token, _} = Guardian.encode_and_sign(user)
-    cms_author = OntagCore.CMS.ensure_author_exists(user)
-    {:ok, entry} = OntagCore.CMS.create_entry(cms_author, %{title: "Example entry"})
-    qams_author = QAMS.ensure_author_exists(user)
-    {:ok, tag1} = QAMS.create_tag(qams_author, %{title: "Tag 1"})
-    {:ok, q1} = QAMS.create_question(qams_author, %{title: "Tag 1"})
-
-    annotations_params = %{
-      tag_id: tag1.id,
-      entry_id: entry.id,
-      target: %{something: ""}
-    }
-    {:ok, a1} = QAMS.create_annotation(qams_author, annotations_params)
-    {:ok, a2} = QAMS.create_annotation(qams_author, annotations_params)
-
-    answer_params = %{
-      question_id: q1.id,
-      annotations: [a1.id, a2.id]
+  test "POST /answers with right parameters", %{valid_token: conn,
+                                                annotation: annotation,
+                                                question: question} do
+    params = %{
+      question_id: question.id,
+      annotations: [annotation.id]
     }
 
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "Bearer #{token}")
-      |> post(answer_path(build_conn(), :create, answer_params))
-
+    conn = post(conn, answer_path(build_conn(), :create, params))
     assert json_response(conn, :created)
   end
 end
